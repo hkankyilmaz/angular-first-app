c7fa504b659119636f628427be126cdc6c9db92d
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2024-06-02 15:15:34 +0300

    search features added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,8 +11,14 @@ import { HousingService } from "../housing.service";
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filter.value)"
+        >
+          Search
+        </button>
       </form>
     </section>
     <section class="results">
@@ -26,8 +32,21 @@ import { HousingService } from "../housing.service";
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[];
+  filteredLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
